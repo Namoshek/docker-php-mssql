e3daab1e5e73c773e32ab095515aed235244101c
Marvin Mall <marvin.mall@energie4all.at>
2020-06-17 14:37:35 +0200

    Replace scripts with GitHub Action

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for version in */ ; do
-    for path in $version/*/ ; do
-        slug=${path/\/\//-}
-        tag=${slug::-1}
-        docker build -t namoshek/php-mssql:$tag $path
-    done
-done
