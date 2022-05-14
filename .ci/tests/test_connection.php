cf87578674e43a7b3895ae8e88bf87a6b74428cd
Namoshek <Namoshek@users.noreply.github.com>
2022-05-14 19:22:55 +0200

    Test connection to SQL Server with built images during CI (#2)
    * Add custom action to create and publish Docker image
    
    * Try using new custom action for one job
    
    * Try using relative path for custom action
    
    * Run CI for PRs but do not push built images
    
    * Add dependabot config for github-actions
    
    * Use new composite action for all jobs and exclude swoole images in PR builds
    
    * Extend composite action to job which also tests the built image
    
    * Checkout repository before running local action
    
    * Pass secrets to composite action as inputs
    
    * Start SQL Server during build before building the image to give it time to start
    
    * Run sqlsrv as service container
    
    * Improve test script to print errors when failing
    
    * Wrap --health-cmd in quotes
    
    * Use different quotes
    
    * Use full path to sqlcmd
    
    * Try to connect to sqlsrv via host.docker.internal
    
    * Connect to sqlsrv via container name
    
    * Use reusable workflow to simplify workflows
    
    * Rename workflow and build
    
    * Remove obsolete action
    
    * Enable remaining builds
    
    * Rename workflows

---
+++ .ci/tests/test_connection.php
@@ -0,0 +1,86 @@
+<?php
+
+function test_sqlsrv_connection(&$errors)
+{
+    $host = getenv('MSSQL_HOST');
+    $port = getenv('MSSQL_PORT');
+
+    $connection = sqlsrv_connect("${host},${port}", [
+        'UID' => getenv('MSSQL_USERNAME'),
+        'PWD' => getenv('MSSQL_PASSWORD'),
+        'Database' => getenv('MSSQL_DATABASE'),
+    ]);
+
+    if ($connection === false) {
+        $errors[] = json_encode(sqlsrv_errors());
+
+        return false;
+    }
+
+    $success = sqlsrv_query($connection, 'SELECT 1');
+
+    if ($success === false) {
+        $errors[] = json_encode(sqlsrv_errors());
+
+        return false;
+    }
+
+    return true;
+}
+
+function test_pdo_sqlsrv_connection(&$errors)
+{
+    $host = getenv('MSSQL_HOST');
+    $port = getenv('MSSQL_PORT');
+    $database = getenv('MSSQL_DATABASE');
+    $username = getenv('MSSQL_USERNAME');
+    $password = getenv('MSSQL_PASSWORD');
+
+    try {
+        $connection = new PDO("sqlsrv:server=${host},${port};Database=${database};ConnectionPooling=0", $username, $password, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+
+        $success = $connection->query('SELECT 1');
+
+        if ($success === false) {
+            return false;
+        }
+    } catch (PDOException $e) {
+        $errors[] = json_encode($e->getMessage());
+
+        return false;
+    }
+
+    return true;
+}
+
+echo '== PHP extensions ==' . PHP_EOL;
+echo 'sqlsrv version: ' . phpversion('sqlsrv') . PHP_EOL;
+echo 'pdo_sqlsrv version: ' . phpversion('pdo_sqlsrv') . PHP_EOL;
+echo PHP_EOL;
+
+echo '== Environment variables ==' . PHP_EOL;
+echo 'MSSQL_HOST: ' . getenv('MSSQL_HOST') . PHP_EOL;
+echo 'MSSQL_PORT:' . getenv('MSSQL_PORT') . PHP_EOL;
+echo 'MSSQL_USERNAME:' . getenv('MSSQL_USERNAME') . PHP_EOL;
+echo 'MSSQL_PASSWORD:' . getenv('MSSQL_PASSWORD') . PHP_EOL;
+echo 'MSSQL_DATABASE:' . getenv('MSSQL_DATABASE') . PHP_EOL;
+echo PHP_EOL;
+
+$errors = [];
+
+echo '== Testing `sqlsrv` extension ==' . PHP_EOL;
+$sqlsrvSuccess = test_sqlsrv_connection($errors);
+echo 'Establishing connection ' . ($sqlsrvSuccess ? 'successful!' : 'failed.') . PHP_EOL;
+
+echo '== Testing `pdo_sqlsrv` extension ==' . PHP_EOL;
+$pdoSqlsrvSuccess = test_pdo_sqlsrv_connection($errors);
+echo 'Establishing connection ' . ($pdoSqlsrvSuccess ? 'successful!' : 'failed.') . PHP_EOL;
+
+if ($sqlsrvSuccess && $pdoSqlsrvSuccess) {
+    exit(0);
+}
+
+var_dump($errors);
+exit(1);
